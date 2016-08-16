c48f43c3a7de8859ddde0fe7058c4964f43de478
Michael Coyle <mcoyle74@gmail.com>
2016-08-16 15:19:02 -0500

    Used blade syntax for a for loop to update roll-dice view to give ul of anchors that link to another guess, 1-6.

--- resources/views/roll-dice.blade.php
+++ resources/views/roll-dice.blade.php
@@ -5,4 +5,10 @@
 		Guess: {{ $guess }}<br>
 		Match: {{ $match }}
 	</h1>
+	<h3>Guess again:</h3>
+	<ul>
+		@for ($i = 1; $i <= 6; $i++)
+			<li><a href="{{ action('HomeController@rollDice', $i) }}">{{ $i }}</a></li>
+		@endfor
+	</ul>
 @stop
