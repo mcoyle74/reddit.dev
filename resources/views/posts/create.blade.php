a99db253bb8a06315719ce20b54a6b6bf220ab73
Michael Coyle <mcoyle74@gmail.com>
2016-08-18 11:36:46 -0500

    Added bootstrap form-control class to inputs

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,7 +4,7 @@
 		{!! csrf_field() !!}
 		<div class="form-group">
 			<label for="title">Title</label>
-			<input type="text" name="title" value="{{ old('title') }}">
+			<input class="form-control" type="text" name="title" value="{{ old('title') }}">
 			@if ($errors->has('title'))
 				{!!  $errors->first('title', '<span class="alert alert-warning">:message</span>') !!}
 			@endif
@@ -12,7 +12,7 @@
 
 		<div class="form-group">
 			<label for="url">URL</label>
-			<input type="url" name="url" value="{{ old('url') }}">
+			<input class="form-control" type="url" name="url" value="{{ old('url') }}">
 			@if ($errors->has('url'))
 				{!!  $errors->first('url', '<span class="alert alert-warning">:message</span>') !!}
 			@endif
@@ -20,7 +20,7 @@
 
 		<div class="form-group">
 			<label for="content">Content</label>
-			<input type="text" name="content" value="{{ old('content') }}">
+			<input class="form-control" type="text" name="content" value="{{ old('content') }}">
 			@if ($errors->has('content'))
 				{!!  $errors->first('content', '<span class="alert alert-warning">:message</span>') !!}
 				@endif
