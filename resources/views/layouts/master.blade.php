69f484b9d81f7d27a1f40932435175284a91bb42
Michael Coyle <mcoyle74@gmail.com>
2016-08-22 15:43:30 -0500

    Added conditions to dropdown menu and logout button so that they only appear when a user is logged in.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -20,7 +20,7 @@
 <body>
 	<div class="container">
 		<nav class="navbar navbar-default">
-			<div class="container-fluid">
+			<div class="container">
 			<!-- Brand and toggle get grouped for better mobile display -->
 				<div class="navbar-header">
 					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -33,23 +33,22 @@
 				</div>
 
 			<!-- Collect the nav links, forms, and other content for toggling -->
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<div class="collapse navbar-collapse" id="navbar-collapse-1">
 				<ul class="nav navbar-nav navbar-right">
 					<li class="active"><a href="{{ action('Auth\AuthController@getLogin') }}">Login <span class="sr-only">(current)</span></a></li>
-					<li><a href="#">Link</a></li>
-				</ul>
-				<ul class="nav navbar-nav navbar-right">
 					<li><a href="{{ action('Auth\AuthController@getRegister') }}">Register</a></li>
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Menu <span class="caret"></span></a>
-						<ul class="dropdown-menu">
-							<li><a href="/posts/create">Create Post</a></li>
-							<li><a href="#">Edit Profile</a></li>
-							<li><a href="{{ action('Auth\AuthController@getLogout') }}">Logout</a></li>
-							<!-- <li role="separator" class="divider"></li>
-							<li><a href="#">Separated link</a></li> -->
-						</ul>
-					</li>
+					@if (Auth::check())
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Menu <span class="caret"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="/posts/create">Create Post</a></li>
+								<li><a href="#">Edit Profile</a></li>
+								<li><a href="{{ action('Auth\AuthController@getLogout') }}">Logout</a></li>
+								<!-- <li role="separator" class="divider"></li>
+								<li><a href="#">Separated link</a></li> -->
+							</ul>
+						</li>
+					@endif
 				</ul>
 			</div><!-- /.navbar-collapse -->
 			</div><!-- /.container-fluid -->
@@ -59,9 +58,11 @@
 			<div class="alert alert-success">{{ session('message') }}</div>
 		@endif
 
-		<form method="GET" action="{{ action('Auth\AuthController@getLogout') }}">
-			<button type="submit" class="btn btn-default">Logout</button>
-		</form>
+		@if (Auth::check())
+			<form method="GET" action="{{ action('Auth\AuthController@getLogout') }}">
+				<button type="submit" class="btn btn-default">Logout</button>
+			</form>
+		@endif
 	</div>
 
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
