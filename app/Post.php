86e75167d2fc5e2f626a70d8c911e15f09a9c47e
Michael Coyle <mcoyle74@gmail.com>
2016-08-24 09:01:34 -0500

    Created BaseModel with accessors methods to format dates.

--- app/Post.php
+++ app/Post.php
@@ -4,16 +4,16 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Post extends BaseModel
 {
-    public static $rules = [
-            'title' => 'required|max:100',
-            'url' => 'required|max:255',
-            'content' => 'required'
-    ];
+	public static $rules = [
+			'title' => 'required|max:100',
+			'url' => 'required|max:255',
+			'content' => 'required'
+	];
 
-    public function user()
-    {
-    	return $this->belongsTo(User::class, 'created_by');
-    }
+	public function user()
+	{
+		return $this->belongsTo(User::class, 'created_by');
+	}
 }
