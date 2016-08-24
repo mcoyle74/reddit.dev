930882de9c6ed79fff0f8029ae32ae811b7f418b
Michael Coyle <mcoyle74@gmail.com>
2016-08-24 17:31:42 -0500

    Moved Create post anchor to top right of page and added bootstrap btn and btn-primary class.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,22 +2,22 @@
 @section('content')
 	<form method="GET" class="form-inline pull-right">
 		<div class="form-group">
-			<input name="keyword" type="text" class="form-control" placeholder="Search post titles">
+			<input name="keyword" type="text" class="form-control" placeholder="Search posts">
 		</div>
 		<button type="submit" class="btn btn-default">Search</button>
 	</form>
+	<div class="pull-right">
+		<a class="btn btn-primary" href="{{ action('PostsController@create') }}">Create a Post</a>
+	</div>
 	<div class="">
 		@foreach ($posts as $post)
 			<div class="">
-				<a href="{{ action('PostsController@show') }}" class="title">{{ $post->title }}</a>
+				<a href="{{ action('PostsController@show', [$post->id]) }}" class="title">{{ $post->title }}</a>
 				<p>submitted {{ $post->created_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:i:s A') }} by {{ $post->user->name }}</p>
 			</div>
 		@endforeach
 	</div>
 	<div class="text-center">
 		{!! $posts->render() !!}
-	</div>
-	<div>
-		<a href="{{ action('PostsController@create') }}">Create a Post</a>
 	</div>
 @stop
