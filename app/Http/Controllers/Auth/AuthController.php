e9b144d0d26a96c1bd8a5c783052e21702e5218a
Michael Coyle <mcoyle74@gmail.com>
2016-08-24 18:25:37 -0500

    Changed redirect on logout to /posts.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -24,6 +24,7 @@ class AuthController extends Controller
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
     protected $redirectPath = '/posts';
+    protected $redirectAfterLogout = '/posts';
 
     /**
      * Create a new authentication controller instance.
