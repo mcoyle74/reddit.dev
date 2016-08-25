3453550993bf2d874deaab624d59f09c2c8a980d
Michael Coyle <mcoyle74@gmail.com>
2016-08-25 16:02:58 -0500

    Removed arrows from index view and styled submit new posts button.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,17 +6,12 @@
 		</div>
 		<button type="submit" class="btn btn-default">Search</button>
 	</form>
-	<div class="pull-right">
-		<a class="btn btn-primary" href="{{ action('PostsController@create') }}">Create a Post</a>
+	<div class="pull-right" id="create-post-div">
+		<a class="btn btn-primary" id="create-post-btn" href="{{ action('PostsController@create') }}">Submit a New Post</a>
 	</div>
 	<div class="">
 		@foreach ($posts as $post)
 			<div class="row">
-				<div class="vote col-md-1">
-					<a href=""><span class="glyphicon glyphicon-arrow-up"></span></a>
-					<p class="count">4</p>
-					<a href=""><span class="glyphicon glyphicon-arrow-down"></span></a>
-				</div>
 				<div class="post col-md-8">
 					<a href="{{ action('PostsController@show', [$post->id]) }}" class="title">{{ $post->title }}</a>
 					<p class="date-and-user">submitted {{ $post->created_at->diffForHumans() }} by {{ $post->user->name }}</p>
