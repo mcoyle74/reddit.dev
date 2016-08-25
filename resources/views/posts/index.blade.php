4d49753c67f3ef8aab09cccebf8927903628febc
Michael Coyle <mcoyle74@gmail.com>
2016-08-25 14:18:59 -0500

    Added up and down vote arrows and counter to posts view and added some CSS for positioning.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,14 +11,16 @@
 	</div>
 	<div class="">
 		@foreach ($posts as $post)
-			<div class="vote">
-				<a href=""><span class="glyphicon glyphicon-arrow-up"></span></a>
-				<p class="count">4</p>
-				<a href=""><span class="glyphicon glyphicon-arrow-down"></span></a>
-			</div>
-			<div class="post">
-				<a href="{{ action('PostsController@show', [$post->id]) }}" class="title">{{ $post->title }}</a>
-				<p>submitted {{ $post->created_at->diffForHumans() }} by {{ $post->user->name }}</p>
+			<div class="row">
+				<div class="vote col-md-1">
+					<a href=""><span class="glyphicon glyphicon-arrow-up"></span></a>
+					<p class="count">4</p>
+					<a href=""><span class="glyphicon glyphicon-arrow-down"></span></a>
+				</div>
+				<div class="post col-md-8">
+					<a href="{{ action('PostsController@show', [$post->id]) }}" class="title">{{ $post->title }}</a>
+					<p class="date-and-user">submitted {{ $post->created_at->diffForHumans() }} by {{ $post->user->name }}</p>
+				</div>
 			</div>
 		@endforeach
 	</div>
