c26e14ff4e2ed7cdc73d287b2cb92fe71453eead
Michael Coyle <mcoyle74@gmail.com>
2016-08-25 16:25:02 -0500

    Changed show view from table to divs with heading tags, anchors, p tags, etc.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,31 +1,22 @@
 @extends('layouts.master')
 @section('content')
-	<table class="table table-striped">
-		<tr>
-			<th>Title</th>
-			<th>URL</th>
-			<th>Content</th>
-			<th>Created</th>
-			@if ($post->ownedBy(Auth::user()))
-				<th>Edit/Delete</th>
-			@endif
-		</tr>
-		<tr>
-			<td>{{ $post->title }}</td>
-			<td>{{ $post->url }}</td>
-			<td>{{ $post->content }}</td>
-			<td>{{ $post->created_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:i:s A') }}</td>
-			@if ($post->ownedBy(Auth::user()))
-				<td>
-					<a href="{{ action('PostsController@edit', $post->id) }}" class="btn btn-primary">Edit</a><br>
-					<form method="POST" action="{{ action('PostsController@destroy', $post->id) }}">
+	<div class="row">
+
+		<div class="">
+			<h1>{{ $post->title }}</h1>
+			<h2>Posted by: {{ $post->user->name }} <small>{{ $post->created_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:i:s A') }}</small></h2>
+			<a href="{{ $post->url }}">{{ $post->url }}</a>
+			<p>{{ $post->content }}</p>
+		</div>
+		@if ($post->ownedBy(Auth::user()))
+			<div class="">
+				<a href="{{ action('PostsController@edit', $post->id) }}" class="btn btn-primary">Edit</a><br>
+				<form method="POST" action="{{ action('PostsController@destroy', $post->id) }}">
 					{{ csrf_field() }}
 					{{ method_field('DELETE') }}
 					<button class="btn btn-danger">Delete</button>
 				</form>	
-				</td>
-				
-			@endif
-		</tr>
-	</table>
+			</div>
+		@endif
+	</div>
 @stop
