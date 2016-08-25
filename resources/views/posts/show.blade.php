6c2c43504c2e1a9286f9bd02194bebf499866d80
Michael Coyle <mcoyle74@gmail.com>
2016-08-25 15:00:05 -0500

    Added blade condition so that only owners of posts may edit or delete.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,17 +6,21 @@
 			<th>URL</th>
 			<th>Content</th>
 			<th>Created</th>
-			<th>Edit/Delete</th>
+			@if (Auth::check())
+				<th>Edit/Delete</th>
+			@endif
 		</tr>
 		<tr>
 			<td>{{ $post->title }}</td>
 			<td>{{ $post->url }}</td>
 			<td>{{ $post->content }}</td>
 			<td>{{ $post->created_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:i:s A') }}</td>
-			<td>
-				<a href="{{ action('PostsController@edit', [$post->id]) }}">Edit</a><br>
-				<a href="/posts/{{$post->id}}">Delete</a>
-			</td>
+			@if (Auth::check())
+				<td>
+					<a href="{{ action('PostsController@edit', [$post->id]) }}">Edit</a><br>
+					<a href="/posts/{{$post->id}}">Delete</a>
+				</td>
+			@endif
 		</tr>
 	</table>
 @stop
