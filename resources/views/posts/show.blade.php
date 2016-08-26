d3336613b416ff66a3de99ac8c3a5b283f85d0f9
Michael Coyle <mcoyle74@gmail.com>
2016-08-26 10:02:56 -0500

    Added and styled vote arrows and score in show view.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,11 +2,22 @@
 @section('content')
 	<div class="row">
 
-		<div class="">
+		<div class="col-sm-8">
 			<h1>{{ $post->title }}</h1>
 			<h2>Posted by: {{ $post->user->name }} <small>{{ $post->created_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:i:s A') }}</small></h2>
 			<a href="{{ $post->url }}">{{ $post->url }}</a>
 			<p>{{ $post->content }}</p>
+		</div>
+		<div class="col-sm-4" id="vote-div">
+			<div class="row">
+				<img src="/img/arrow-up.png" class="img-responsive center-block">
+			</div>
+			<div class="row">
+				<p class="text-center" id="vote-score">4</p>
+			</div>
+			<div class="row">
+				<img src="/img/arrow-down.png" class="img-responsive center-block">
+			</div>
 		</div>
 		@if ($post->ownedBy(Auth::user()))
 			<div class="">
